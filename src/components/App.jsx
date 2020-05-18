b4da9fd9960e9bbf06f90f858d3735f301e05db9
Michael <kllmmc23@gmail.com>
2020-05-18 01:50:24 -0600

    reduce-map-filter

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import emojipedia from "../emojipedia.js";
-import createEntry from "./createEntry";
-
+import Entry from "./Entry";
 export default function App() {
   return (
     <div>
       <h1>
         <span>emojipedia</span>
       </h1>
-      <dl className="dictionary">{emojipedia.map(createEntry)}</dl>
+      <dl className="dictionary">
+      {emojipedia.map(term  => (
+        <Entry
+          key={term.id}
+          emoji={term.emoji}
+          name={term.name}
+          meaning={term.meaning}
+    />))}
+    </dl>
     </div>
   );
 }
